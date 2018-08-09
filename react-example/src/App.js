92fcc783ebf7f09604105915010f5e22928156c6
Peter Cho <pcho231@gmail.com>
2018-08-09 01:11:36 -0400

    Staging changes for talk

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -9,16 +9,26 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+Welcome to React
+          </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          {' '}
+          <code>
+src/App.js
+          </code>
+          {' '}
+and save to reload.
   asdf
      asdf
  asdf
 asdf
         </p>
-        <a href="https://www.askjeeves.com" target='_blank'>asdf</a>
+        <a href="https://www.askjeeves.com" target="_blank">
+asdf
+        </a>
       </div>
     );
   }
